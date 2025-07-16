d905de5b70c212905f57802caac89e7a35728ca9
nguyendangkin <nguyenchin0077@gmail.com>
2025-07-16 21:47:57 +0700

    up

---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+import HomeCompo from "@/components/home/HomeCompo";
+import Image from "next/image";
+
+export default function Home() {
+    return <HomeCompo />;
+}
---
+++ src/components/home/HomeCompo.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { Button } from "antd";
+
+export default function HomeCompo() {
+    return (
+        <div>
+            <Button onClick={() => alert("hello")}>xin chào</Button>
+        </div>
+    );
+}
